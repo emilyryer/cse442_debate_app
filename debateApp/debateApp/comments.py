cde5c422576d4ed9914a2e607f77433059de4910
Emily Ryer <emilyrye@buffalo.edu>
2019-03-01 14:01:43 -0500

    Update comments.py

--- debateApp/debateApp/comments.py
+++ debateApp/debateApp/comments.py
@@ -1,15 +1,14 @@
 import logging
 import os
-import main.py
+import rooms.py
 
 from google.cloud import storage
-from flask import Flask
-
-commentThread = list
+from flask import (Flask, render_template)
 
 def commentToFile(commentText):
-    f= open("comment.txt","w+")
-    f.write(commentText)
+    f= open("comment.csv","w+")
+
+    f.write(commentText + ", ")
     f.close()
     return f
 
@@ -23,9 +22,9 @@ def createComment(roomBucket, commentText):
     return
 
 def reply(roomBucket, comment, replyText):
-    comment = get_buckets(room)
-    f= open("comment.txt","w+")
-    f.write(commentText)
+    comment = get_buckets(roomBucket)
+    f= open("comment.txt","w")
+    f.write(replyText + ",")
     f.close()
 
 
