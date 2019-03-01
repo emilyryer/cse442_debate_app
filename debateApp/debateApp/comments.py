3fb942bfde91002fa791dc5e3bbcb64b708b6436
Emily Ryer <emilyrye@buffalo.edu>
2019-02-28 22:17:00 -0500

    Create comments.py

---
+++ debateApp/debateApp/comments.py
@@ -0,0 +1,14 @@
+import logging
+import os
+
+from google.cloud import storage
+from flask import Flask
+
+commentThread = list
+
+
+def createComment():
+    return commentThread
+
+def reply():
+    return commentThread
