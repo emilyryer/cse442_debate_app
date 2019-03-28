26ccfcbd82137fcc86caf1c8c2ad5b65064c4c82
Emily Ryer <emilyrye@buffalo.edu>
2019-03-28 17:34:42 -0400

    Edited system test to be specific to our app

--- selenium_tests/chrome/system_test_chrome.py
+++ selenium_tests/chrome/system_test_chrome.py
@@ -3,14 +3,10 @@ from selenium import webdriver
 
 #This test is to show that you've properly set up selenium on your system.
 driver = webdriver.Chrome('chromedriver.exe')  # chromedriver.exe is included in the selenium_tests/chrome folder
-driver.get('http://www.google.com/xhtml');
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('University at buffalo') #Types this text into the search_box element
-search_box.submit()
+driver.get('http://127.0.0.1:8000/');
 time.sleep(5) # Let the user actually see something!
 
 html = driver.page_source
-print(html) #this is going to be painfully long, but it makes sense, I swear
+print(html) #This should be the source for the page
 
 driver.quit() #terminates program
