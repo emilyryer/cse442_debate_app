fa22f40a05d9297c1c8d0ac6c35205864b2456ea
Emily Ryer <emilyrye@buffalo.edu>
2019-03-28 16:45:22 -0400

    Changed folder structure to organize by which browser the tests are for

--- selenium_tests/chrome/system_test_chrome.py
+++ selenium_tests/chrome/system_test_chrome.py
@@ -1,11 +1,12 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('chromedriver.exe')  # Optional argument, if not specified will search path.
+#This test is to show that you've properly set up selenium on your system.
+driver = webdriver.Chrome('chromedriver.exe')  # chromedriver.exe is included in the selenium_tests/chrome folder
 driver.get('http://www.google.com/xhtml');
 time.sleep(5) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
+search_box.send_keys('University at buffalo') #Types this text into the search_box element
 search_box.submit()
 time.sleep(5) # Let the user actually see something!
 
