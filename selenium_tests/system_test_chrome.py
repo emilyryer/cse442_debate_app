7d6840f3493043fca9a0f7f3e2e51d17a02d82bd
Emily Ryer <emilyrye@buffalo.edu>
2019-03-28 16:29:14 -0400

    Added basic test for chrome
    This is a slightly modified test from the webdriver tutorial. It tests whether or not selenium is installed properly on your system and does a few basic functions.

---
+++ selenium_tests/system_test_chrome.py
@@ -0,0 +1,14 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome('chromedriver.exe')  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/xhtml');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+
+html = driver.page_source
+print(html) #this is going to be painfully long, but it works, I swear
+driver.quit()
