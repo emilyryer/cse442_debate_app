4aa6eb3cee2962500ea50d9567e451c87ecaeda1
Emily Ryer <emilyrye@buffalo.edu>
2019-04-11 20:29:12 -0400

    updated the last of the urls to the deployed version
    All automated browser tests now use the deployed version of the app, and no longer run locally.

--- selenium_tests/chrome/join_room_test.py
+++ selenium_tests/chrome/join_room_test.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 import test_functions
 
 driver = webdriver.Chrome('chromedriver.exe')  # chromedriver.exe is included in the selenium_tests/chrome folder
-driver.get('http://127.0.0.1:8000/');
+driver.get('https://zippy-hold-232119.appspot.com/');
 time.sleep(2) # Let the user actually see something!
 
 email1 = test_functions.generateTestEmail()
