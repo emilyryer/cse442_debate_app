a1b103f80d8981742d963a81a4fe9da2c07c24da
Khari Seals <kseals281@gmail.com>
2019-04-12 15:37:11 -0400

    Added the changes over the past few commits manually because of an unknown bug causing creds.json to not be found

--- rooms/tests/rooms_test.py
+++ rooms/tests/rooms_test.py
@@ -1,22 +1,21 @@
-import unittest
-import logging
 import rooms
+import pytest
 
-from google.cloud import storage
+from google.cloud import (storage, exceptions)
 
 client = storage.Client.from_service_account_json('creds.json')
-logging.basicConfig(filename='test_room.log', level='INFO', format='w')
 
-class TestRooms(unittest.TestCase):
+def test_create_room():
+    retVal = rooms.create_room(room_name='Test Room', topic='test', user='unknown')
+    assert retVal == 'NEW BUCKET CREATED: Test Room\nOWNER: unknown\nTOPIC: test'
 
-	def test_add_label(self):
-		logging.info('Starting label test')
-		buckets = client.list_buckets(prefix='label-test')
-		try:
-			bucket =
-		except Exception as e:
-			raise e
-		create_room('label-test', 'testing the label maker')
+#def test_join_room():
+#    room = client.lookup_bucket('Test Room')
+#    labels = room.labels
+#    join_code = labels[1]
+#    joined_room = rooms.join_room('Test Room', join_code)
+#    assert joined_room == room
 
-if __name__ == '__main__':
-    unittest.main()
+#def test_delete_room():
+#    rooms.delete_room('Test Room')
+#    assert client.lookup_bucket(new_bucket_name) == None
