80dc68d8e67810e10773dd91d492badd108978a2
Emily Ryer <emilyrye@buffalo.edu>
2019-04-26 14:45:45 -0400

    Rename of profile picture module and addition of bucket methods
    Began work on methods for getting profile picture from a bucket

--- profile_picture/profile_picture.py
+++ profile_picture/profile_picture.py
@@ -2,9 +2,10 @@ from PIL import Image, ImageDraw, ImageFont
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkcolorpicker import askcolor
+#from google.cloud import storage
 #Install Pillow with pip install pillow
 
-def draw_profile_pic(name):
+def draw_profile_pic(name, email):
     initial = ''
     if len(name) == 1:
         initial = name
@@ -34,8 +35,26 @@ def draw_profile_pic(name):
     if (redInt + greenInt + blueInt) < 450:
         textRGB = 255
     draw.text((120,40),initial,(textRGB,textRGB,textRGB),font=font)
-    img.save(name + '.png')
+    emailFilename = email.replace('.', '')
+    emailFilename = emailFilename.replace('@', '')
+    filename = emailFilename + '.jpg'
+    img.save(filename)
+    return filename
+
+def profile_picture_to_bucket(filename):
+    storage_client = storage.Client()
+    bucket_name = 'debate_app_profile_pics'
+    bucket = storage_client.get_bucket(bucket_name)
+    bucket.upload_from_filename(filename)
+
+def get_profile_pic(email):
+    storage_client = storage.Client()
+    emailFilename = email.replace('.', '')
+    emailFilename = emailFilename.replace('@', '')
+    bucket_name = 'debate_app_profile_pics'
+    bucket = storage_client.get_bucket(bucket_name)
+    
 
 #Test Cases (Uncomment to run)
-draw_profile_pic('A') #case for single letter nickName
-draw_profile_pic('BBBB') #case for multiple letter nickname
+draw_profile_pic('A', 'test@test.com') #case for single letter nickName
+draw_profile_pic('BBBB', 'test2@test.com') #case for multiple letter nickname
